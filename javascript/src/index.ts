4b2f42aa2bc70d51b66b4de58f7a1a435d3b124e
Claus Höfele <claus@claushoefele.com>
2026-03-25 11:22:52 +0100

    More playing with JS

--- javascript/src/index.ts
+++ javascript/src/index.ts
@@ -59,3 +59,74 @@ console.log("---");
 //     ? sorted[mid]
 //     : (sorted[mid - 1] + sorted[mid]) / 2;
 // }
+
+// function createH1Element() {
+//   const h1 = document.createElement("h1");
+//   h1.textContent = "Hello world!";
+//   h1.style.color = "red";
+//   document.body.appendChild(h1);
+// }
+
+// const createH1btn = document.getElementById("h1btn");
+// createH1btn?.addEventListener("click", createH1Element);
+// console.log("h1btn");
+// console.log(createH1btn);
+
+// fetch("https://fakestoreapi.com/products")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+// (async () => {
+//   const data = await fetch("https://fakestoreapi.com/products");
+//   const json = await data.json();
+//   console.log(json);
+// })();
+
+type Product = {
+  title: string;
+};
+
+async function fetchData() {
+  const data = await fetch("https://fakestoreapi.com/products");
+  if (data.ok) {
+    const products: Product[] = await data.json();
+    products
+      .map((product) => {
+        const h2 = document.createElement("h2");
+        h2.textContent = product.title;
+        return h2;
+      })
+      .forEach((element) => document.body.appendChild(element));
+  } else {
+    const p = document.createElement("p");
+    p.textContent = `${data.status} - ${data.statusText}`;
+    p.style.color = "red";
+    document.body.appendChild(p);
+  }
+}
+
+const createH1btn = document.getElementById("h1btn");
+createH1btn?.addEventListener("click", fetchData);
+
+// ---
+type Person = {
+  name: string;
+  age: number;
+};
+type Data = {
+  persons: Person[];
+};
+
+const data: Data = {
+  persons: [
+    { name: "C", age: 32 },
+    { name: "D", age: 16 },
+  ],
+};
+
+localStorage.setItem("data", JSON.stringify(data));
+const readData: Data = JSON.parse(localStorage.getItem("data") || "");
+console.log(readData);
+
+const name = readData.persons[0]?.name;
+console.log(name);
