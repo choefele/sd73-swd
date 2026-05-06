4ba524dbd8dbd71a3c01209eb276dfa18c788690
Claus Höfele <claus@claushoefele.com>
2026-05-06 11:24:43 +0200

    Remove MongoDB-specific properties in API response

--- express/src/models/user.ts
+++ express/src/models/user.ts
@@ -1,6 +1,18 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: function (doc) {
+        const { __v, _id, ...plain } = doc.toObject();
+        return plain;
+      },
+    },
+    toObject: { virtuals: true },
+  },
+);
 export default model("User", userSchema);
