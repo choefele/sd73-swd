5d00ea578954ac1ea7f1eaed383e0b9770155039
Claus Höfele <claus@claushoefele.com>
2026-05-08 17:28:49 +0200

    Remove obsolete error handling

--- express/src/users/controllers.ts
+++ express/src/users/controllers.ts
@@ -2,23 +2,13 @@ import type { RequestHandler } from "express";
 import { UserModel } from "./model.js";
 
 export const createUser: RequestHandler = async (req, res) => {
-  try {
-    const user = await UserModel.create(req.body);
-    res.status(201);
-    res.json(user);
-  } catch (error) {
-    res.status(400);
-    res.json({ message: "Invalid input data" });
-  }
+  const user = await UserModel.create(req.body);
+  res.status(201);
+  res.json(user);
 };
 
 export const readUsers: RequestHandler = async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.status(200);
-    res.json(users);
-  } catch (error) {
-    res.status(400);
-    res.json({ message: "Invalid input data" });
-  }
+  const users = await UserModel.find();
+  res.status(200);
+  res.json(users);
 };
